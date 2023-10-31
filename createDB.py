31a9266db47530a224a934cbe40c6fa16f73b012
Sandesh Tidake0408 <sandeshtidake37@gmail.com>
2023-10-31 16:25:05 +0530

    result add successfully

--- createDB.py
+++ createDB.py
@@ -7,7 +7,10 @@ def create_db():
     cursor.execute(
         "create table if not exists course(cid integer primary key autoincrement,name text,duration text, charges text, description text)")
     con.commit()
-    cursor.execute("create table if not exists student(roll integer primary key autoincrement, name text, email, gender text, dob text, contact text,admission text, course text, state text, city text, pincode text, address text)")
+    cursor.execute("create table if not exists student(roll integer primary key autoincrement, name text, email text, gender text, dob text, contact text,admission text, course text, state text, city text, pincode text, address text)")
+    con.commit()
+    cursor.execute(
+        "create table if not exists result(rid integer primary key autoincrement,roll text, name text, course text, marks_ob text,full_marks text, per text)")
     con.commit()
     con.close()
 
--- results.py
+++ results.py
@@ -35,13 +35,14 @@ class ResultClass:
 
         # Entry Field------------------------
         self.roll_list = ["Select"]
+        self.fetch_roll()
 
         self.txt_student = ttk.Combobox(self.root, textvariable=self.var_roll, values=self.roll_list, font=(
             "Mukta", 15), state='readonly', justify=CENTER)
         self.txt_student.place(x=270, y=105, width=140)
         self.txt_student.current(0)
         self.btn_select = Button(self.root, text="Select", font=(
-            "Mukta", 15, "bold"), bg="#374151", fg="white", borderwidth=0, relief="flat", highlightthickness=0,  cursor="hand2").place(x=420, y=105, width=100, height=30)
+            "Mukta", 15, "bold"), bg="#374151", fg="white", borderwidth=0, relief="flat", highlightthickness=0,  cursor="hand2", command=self.search).place(x=420, y=105, width=100, height=30)
 
         txt_name = Entry(self.root, textvariable=self.var_name, font=(
             "Mukta", 15),  bg="lightyellow", fg="black").place(x=270, y=165, width=250)
@@ -56,9 +57,9 @@ class ResultClass:
 
         # button=============
         btn_add = Button(self.root, text="Submit", font=("Mukta", 15), bg="lightgreen",
-                         activebackground="lightgreen", cursor="hand2", borderwidth=0, relief="flat", highlightthickness=0).place(x=270, y=420, width=120, height=35)
+                         activebackground="lightgreen", cursor="hand2", borderwidth=0, relief="flat", highlightthickness=0, command=self.add).place(x=270, y=420, width=120, height=35)
         btn_clear = Button(self.root, text="Clear", font=("Mukta", 15), bg="lightgray",
-                           activebackground="lightgray", cursor="hand2", borderwidth=0, relief="flat", highlightthickness=0).place(x=400, y=420, width=120, height=35)
+                           activebackground="lightgray", cursor="hand2", borderwidth=0, relief="flat", highlightthickness=0, command=self.clear).place(x=400, y=420, width=120, height=35)
         # image============
         self.bg_img = Image.open("images/result.jpg")
         self.bg_img = self.bg_img.resize(
@@ -67,6 +68,82 @@ class ResultClass:
 
         self.bg_label = Label(self.root, image=self.bg_img, ).place(
             x=630, y=100)
+
+  # functions started================
+
+    def fetch_roll(self):
+        con = sqlite3.connect(database="rms.db")
+        cursor = con.cursor()
+        try:
+            cursor.execute("select roll from student")
+            rows = cursor.fetchall()
+            if (len(rows) > 0):
+                for row in rows:
+                    self.roll_list.append(row[0])
+        except Exception as ex:
+            messagebox.showerror("Error", "Error due to {str(ex)}")
+
+    def search(self):
+        con = sqlite3.connect(database="rms.db")
+        cursor = con.cursor()
+        try:
+            cursor.execute("select name ,course from student where roll=?",
+                           (self.var_roll.get(),))
+            row = cursor.fetchone()
+            if row != None:
+                self.var_name.set(row[0])
+                self.var_course.set(row[1])
+            else:
+                messagebox.showerror(
+                    "Error", "No Record Found", parent=self.root)
+
+        except Exception as ex:
+            messagebox.showerror("Error", "Error due to {str(ex)}")
+
+    def add(self):
+        con = sqlite3.connect(database="rms.db")
+        cursor = con.cursor()
+        try:
+            if self.var_name.get() == "":
+                messagebox.showerror(
+                    "Eror", "Please First Select Record", parent=self.root)
+            else:
+                cursor.execute("select *from result where roll=? and course=?",
+                               (self.var_roll.get(), self.var_course.get()))
+                row = cursor.fetchone()
+
+                if row != None:
+                    messagebox.showerror(
+                        "Error", "Result already present", parent=self.root)
+                else:
+                    marks = int(self.var_marks.get())
+                    total_marks = int(self.var_fullmarks.get())
+                    percentage = (marks*100)/total_marks
+
+                    insert_query = "INSERT INTO result (roll, name, course, marks_ob, full_marks, per) VALUES (?, ?, ?, ?, ?, ?)"
+                    data = (
+                        self.var_roll.get(),
+                        self.var_name.get(),
+                        self.var_course.get(),
+                        self.var_marks.get(),
+                        self.var_fullmarks.get(),
+                        str(percentage))
+
+                    cursor.execute(insert_query, data)
+
+                    con.commit()
+                    messagebox.showinfo(
+                        "Success", "Result Added Successfully", parent=self.root)
+
+        except Exception as ex:
+            messagebox.showerror("Error", f"Error due to {str(ex)}")
+
+    def clear(self):
+        self.var_roll.set('Select')
+        self.var_name.set('')
+        self.var_course.set('')
+        self.var_marks.set('')
+        self.var_fullmarks.set('')
 
 
 if __name__ == "__main__":
